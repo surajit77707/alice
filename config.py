d38738d332c87e37ce1559ed7b7136e2b064a326
surajit77707 <surajitsingh77707@gmail.com>
2025-02-05 19:48:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = "522263f5cebb8233c42363cc0d2624c4"
 BOT_TOKEN = "8061490595:AAFQIcSHNujuMUUWo0S3dUTj_8rk5Zi5sbs"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "mongodb+srv://surajitsingh4203:surajitsingh4203@cluster0.62ig0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://surajitsinghcd123:surajitsinghcd123@venomy.4nwa5.mongodb.net/?retryWrites=true&w=majority&appName=venomy"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
